3788df8d96bfde7ba373831356611874b83d16e5
Wellington Ramos Nogueira <wellington.nogueira@gmail.com>
2013-07-22 03:21:58 -0300

    atualização

--- src/br/com/amil/predojo/Match.java
+++ src/br/com/amil/predojo/Match.java
@@ -1,10 +1,41 @@
 package br.com.amil.predojo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Match {
+/**
+ * Classe que representa a rodada.
+ * 
+ * @author wellington.nogueira@gmail.com
+ *
+ */
+public class Match implements Runnable {
 	
-	long id;
-	List<Player> players;
+	private List<Player> playersList = new ArrayList<Player>();
+	private long matchId;
+	
+	public Match(Player... players) {
+		this.playersList.addAll(Arrays.asList(players));
+	}
+	
+	public void start() {
+		long id = System.currentTimeMillis() % 10000000;
+		this.matchId = id;
+		System.out.println("New match " + id + " has started");
+		
+		run();
+	}
+	
+	@Override
+	public void run() {
+		
+		//Run the match
+		
+	}
+	
+	public void end() {
+		System.out.println("Match " + this.matchId + " has ended");
+	}
 
 }
